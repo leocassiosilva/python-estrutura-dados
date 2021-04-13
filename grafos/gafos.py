23d47907adef0a71123b9e1c9ce8b7a3b70f1c64
leocassiosilva <leocassiosilva1234@gmail.com>
2021-04-13 20:06:38 -0300

    grafos 1

--- grafos/gafos.py
+++ grafos/gafos.py
@@ -2,16 +2,17 @@ class Grafo():
 
     def __init__(self, vertices):
         self.vertices = vertices
-        self.grafo = [[0] * vertices for i in range(vertices)]
+        self.grafo = [[] for i in range(vertices)]
 
     def add_aresta(self, u, v):
-        self.grafo[u - 1][v - 1] = 1
-        self.grafo[v - 1][u - 1] = 1
+        self.grafo[u - 1].append(v - 1)
+        
 
     def show(self):
-        for i  in self.grafo:
-            for j in i:
-                print(j, end = ' ')
+        for i  in range(self.vertices):
+            print('%d: ' % (i + 1), end=' ')
+            for j in self.grafo[i]:
+                print('%d ->' % (j + 1), end = ' ')
             print(' ')
 
 
