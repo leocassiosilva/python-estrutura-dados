25af35959456174494f5100c7566e6166e19089b
leocassiosilva <leocassiosilva1234@gmail.com>
2021-04-14 19:30:47 -0300

    grafos dsf

--- grafos/gafos.py
+++ grafos/gafos.py
@@ -2,10 +2,13 @@ class Grafo():
 
     def __init__(self, vertices):
         self.vertices = vertices
-        self.grafo = [[] for i in range(vertices)]
+        self.grafo = [[0] * vertices for i in range(vertices)]
+        self.visitados = [False] * vertices
+
 
     def add_aresta(self, u, v):
-        self.grafo[u - 1].append(v - 1)
+        self.grafo[u - 1][v - 1] = 1
+        self.grafo[v - 1][u - 1] = 1
         
 
     def show(self):
@@ -15,13 +18,30 @@ class Grafo():
                 print('%d ->' % (j + 1), end = ' ')
             print(' ')
 
+    def tem_licagao (self, u, v):
+        if self.grafo[u - 1][v - 1] == 1:
+            return True
+        return False
+
+    def dfs(self, u):
+        self.visitados[u - 1] = True
+
+        print('%d visitado' % u)
+        for i in range(1, self.vertices + 1):
+            if self.grafo[u - 1][i - 1] == 1 and self.visitados[i - 1] == False:
+                self.dfs(i)
+                    
+    
+
+
 
 g = Grafo(5)
-g.add_aresta(1, 3)
-g.add_aresta(3, 4)
-g.add_aresta(2, 3)
-g.add_aresta(3, 5)
+g.add_aresta(1, 4)
+g.add_aresta(4, 2)
 g.add_aresta(4, 5)
+g.add_aresta(2, 5)
+g.add_aresta(5, 3)
 
-g.show()
+
+g.dfs(1)
         
